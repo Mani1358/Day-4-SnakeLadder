606f9501d753396228afab163842e448177a99f0
Mani1358 <mani621998@gmail.com>
2023-01-28 13:31:05 +0530

    Added:UC-4

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,19 +2,36 @@ package com.Java.snakeladder;
 
 public class SnakeLadder {
     public static void main(String[] args) {
-        int singlePlayerValue = 0;
-        int player = 0;
-        int dice = 1 + (int) (Math.random() * 6);
-        System.out.println("initial playerPosition: " + player);
-        player = player + dice;
-        String option = "" + player;
-        if (option == "No play") {
-            System.out.println("PLAYER POSITION IS SAME");
-        } else if (option == "Ladder") {
-            player = player + 10;
-        } else if (option == "Snake") {
-            player = player - 10;
-        }
-        System.out.println(option + dice);
+        int token1 = 0;
+        int counter1 = 0;
+        int i1 = 1;
+        do {
+            counter1 += i1;
+            System.out.print(token1 + " ");
+            int x1 = (int) (Math.random() * 10) % 3;
+            int dice1 = (int) (Math.random() * 10) % 3;
+            switch (x1) {
+                case 0: {
+                    break;
+                }
+                case 1: {
+                    token1 -= dice1;
+                    if (token1 < 0) {
+                        token1 = 0;
+                    } else if (token1 == 100) {
+                        token1 = 100;
+                    }
+                    break;
+                }
+                default:
+                    token1 += dice1;
+                    if (token1 > 100) {
+                        token1 -= dice1;
+                    }
+            }
+        } while (!(token1 == 100));
+        System.out.println("100");
+        System.out.println(counter1);
+        System.out.println("Winning point Reached");
     }
 }
