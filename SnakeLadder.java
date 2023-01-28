4bb8157705e05fd5090dc1d5b505ae5492c2b49d
Mani1358 <mani621998@gmail.com>
2023-01-28 13:10:28 +0530

    Added:UC-3

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,9 +4,17 @@ public class SnakeLadder {
     public static void main(String[] args) {
         int singlePlayerValue = 0;
         int player = 0;
-        int dice = 1+(int)(Math.random()*6);
+        int dice = 1 + (int) (Math.random() * 6);
         System.out.println("initial playerPosition: " + player);
-        System.out.println(dice);
+        player = player + dice;
+        String option = "" + player;
+        if (option == "No play") {
+            System.out.println("PLAYER POSITION IS SAME");
+        } else if (option == "Ladder") {
+            player = player + 10;
+        } else if (option == "Snake") {
+            player = player - 10;
+        }
+        System.out.println(option + dice);
     }
-
 }
