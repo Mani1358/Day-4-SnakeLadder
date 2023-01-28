e3445994d89b3a9f19fae61f7d840cf028b9f9be
Mani1358 <mani621998@gmail.com>
2023-01-28 12:42:39 +0530

    Added:SnakeLadder program

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.Java.snakeladder;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        int singlePlayerValue = 0;
+        int player = 0;
+        System.out.println("initial playerPosition: " + player);
+    }
+
+}
